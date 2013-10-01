b8147eac5a5b0e2b249ea98bdc9afe74504b338e
Qnex93 <qnexdev@gmail.com>
2013-10-01 09:04:16 +0300

    Add project files

---
+++ src/Program.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Qnex
+ * Date: 01.10.13
+ * Time: 0:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class Program {
+}
