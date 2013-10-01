812d380007dca2d433ce48bff4a9032e66a0c4bf
Qnex93 <qnexdev@gmail.com>
2013-10-01 20:32:03 +0300

    add RegistrationController class

---
+++ src/RegistrationController.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Qnex
+ * Date: 01.10.13
+ * Time: 19:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class RegistrationController  {
+
+}
